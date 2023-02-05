a77aae0b9bbf738edf63c5efd737636b7b86c33a
Ladislus <walcak.ladislas@gmail.com>
2023-02-05 13:34:26 +0100

    [MOD] Added some formatting for log! macro

--- src/utils.rs
+++ src/utils.rs
@@ -3,10 +3,6 @@ macro_rules! log {
     ($prefix:literal, $msg:expr) => {
         // Because macro arguments don't have types, force type by assigning to variable which is typed
         let _p: &str = $prefix;
-
-        let formatted_prefix = format!("[{}]", $prefix);
-        let formatted_location = format!("({}:{})", file!(), line!());
-        let informations = format!("{:<15} {}", formatted_prefix, formatted_location);
-        println!("{:<40}\t{}", informations, $msg);
+        println!("{:<40}\t{}", format!("{:<15} {}", format!("[{}]", $prefix), format!("({}:{})", file!(), line!())), $msg);
     };
 }
