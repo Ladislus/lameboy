b2fc1db68c54504508e89ad9007962ae64695931
Ladislus <walcak.ladislas@gmail.com>
2023-02-06 01:44:06 +0100

    [ADD] Added GenericInstruction enum to allow different parameter type for 'execute' method

--- src/instruction.rs
+++ src/instruction.rs
@@ -11,18 +11,25 @@ pub type InstructionFn<T> = fn(&Instruction<T>, &mut Memory, values: T);
 pub struct Instruction<T> {
     pub opcode: OpCode,
     pub disassembly: &'static str,
+    pub byte_size: usize,
     pub operands_count: usize,
     pub clock_tick: u8,
     pub function: InstructionFn<T>,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub enum GenericInstruction {
+    /// Doesn't take any operand
     VOID(Instruction<()>),
+    /// Takes 8-bit data operand
     D8(Instruction<u8>),
+    /// Takes 16-bit little-endian data
     D16(Instruction<u16>),
+    /// Takes 8-bit data (offset for $FF00)
     A8(Instruction<u8>),
+    /// Takes 16-bit little endian address
     A16(Instruction<u16>),
+    /// Takes 8-bit signed data
     R8(Instruction<i8>)
 }
 
@@ -31,8 +38,9 @@ impl<T> Debug for Instruction<T> {
         f.debug_struct("Instruction")
             .field("OP Code", &format_args!("{:#04X}", self.opcode))
             .field("ASM Name", &self.disassembly)
+            .field("Byte size", &self.byte_size)
             .field("Number of operands", &self.operands_count)
-            .field("Clock ticks", &self.clock_tick)
+            .field("CPU Clock ticks", &self.clock_tick)
             .finish_non_exhaustive()
     }
 }
