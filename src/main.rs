3250a2d305b8fbf59934df5b933c4510ae4e0604
Ladislus <walcak.ladislas@gmail.com>
2023-02-06 01:45:26 +0100

    [MOD] Added Instruction test

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,22 @@ mod operations;
 
 use crate::gui::launch_gui;
 use crate::memory::Memory;
-use crate::instruction::{instruction_from_opcode, GenericInstruction};
+use crate::instruction::{instruction_from_opcode, GenericInstruction, OpCode};
+use crate::operations::INSTRUCTIONS;
 
 fn main() {
     // Allocate 1024 bytes of memory
     let mut memory = Memory::new(1024);
-    match instruction_from_opcode(1) {
-        GenericInstruction::VOID(instr) => instr.execute(&mut memory, ()),
-        GenericInstruction::D16(instr) => instr.execute(&mut memory, 1),
-        _ => unimplemented!("GenericInstruction not implemented")
+
+    // TODO: Remove after test
+    for idx in 0..INSTRUCTIONS.len() {
+        match instruction_from_opcode(idx as OpCode) {
+            GenericInstruction::VOID(instr) => instr.execute(&mut memory, ()),
+            GenericInstruction::D16(instr) => instr.execute(&mut memory, 1),
+            GenericInstruction::D8(instr) => instr.execute(&mut memory, 1),
+            GenericInstruction::A16(instr) => instr.execute(&mut memory, 100),
+            instr @ _ => unimplemented!("GenericInstruction {:?} not implemented", instr)
+        }
     }
 
     launch_gui();
