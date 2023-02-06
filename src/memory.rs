cdb03b5ebf9e1955350e4269209bf61a703f99d7
Ladislus <walcak.ladislas@gmail.com>
2023-02-06 01:42:47 +0100

    [ADD] Added memory access/write utility functions

--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,6 @@
 use crate::register::{Registers, simple_to_wide, wide_to_simple};
 
-const DATA_ADDR_START: usize = 0xFF00;
+const SIMPLE_ADDR_START: usize = 0xFF00;
 
 type Byte = u8;
 
@@ -28,27 +28,60 @@ impl Memory {
         }
     }
 
-    pub fn set_memory_simple(&mut self, addr: usize, value: SimpleValue) {
-        debug_assert!((DATA_ADDR_START + addr) < self.size);
-        self.memory[DATA_ADDR_START + addr] = value;
+    pub fn write_simple_near_addr(&mut self, addr: SimpleValue, value: SimpleValue) {
+        let addr = addr as usize;
+        debug_assert!((SIMPLE_ADDR_START + addr) < self.size);
+        self.memory[SIMPLE_ADDR_START + addr] = value;
     }
 
-    pub fn read_memory_simple(&self, addr: usize) -> SimpleValue {
-        debug_assert!((DATA_ADDR_START + addr) < self.size);
-        return self.memory[DATA_ADDR_START + addr];
+    pub fn write_simple_far_addr(&mut self, addr: WideValue, value: SimpleValue) {
+        let addr = addr as usize;
+        debug_assert!(addr< self.size);
+        self.memory[addr] = value;
     }
 
-    pub fn set_memory_wide(&mut self, addr: usize, value: WideValue) {
-        debug_assert!((DATA_ADDR_START + addr) < self.size);
-        // TODO: Check endianness
+    pub fn read_simple_near_addr(&self, addr: SimpleValue) -> SimpleValue {
+        let addr = addr as usize;
+        debug_assert!((SIMPLE_ADDR_START + addr) < self.size);
+        return self.memory[SIMPLE_ADDR_START + addr];
+    }
+
+    pub fn read_simple_far_addr(&self, addr: WideValue) -> SimpleValue {
+        let addr = addr as usize;
+        debug_assert!(addr < self.size);
+        return self.memory[addr];
+    }
+
+    // TODO: Check endianness
+    pub fn write_wide_near_addr(&mut self, addr: SimpleValue, value: WideValue) {
+        let addr = addr as usize;
+        debug_assert!((SIMPLE_ADDR_START + addr + 1) < self.size);
         let value = wide_to_simple(value);
-        self.set_memory_simple(addr, value.0);
-        self.set_memory_simple(addr + 1, value.1);
+        self.memory[SIMPLE_ADDR_START + addr] = value.0;
+        self.memory[SIMPLE_ADDR_START + addr + 1] = value.1
     }
 
-    pub fn read_memory_wide(&self, addr: usize) -> WideValue {
-        debug_assert!((DATA_ADDR_START + addr) < self.size);
-        // TODO: Check endianness
-        return simple_to_wide(&(self.memory[DATA_ADDR_START + addr], self.memory[DATA_ADDR_START + addr + 1]));
+    // TODO: Check endianness
+    pub fn write_wide_far_addr(&mut self, addr: WideValue, value: WideValue) {
+        let addr = addr as usize;
+        debug_assert!(addr + 1 < self.size);
+        let value = wide_to_simple(value);
+        self.memory[addr] = value.0;
+        self.memory[addr + 1] = value.1
+    }
+
+    // TODO: Check endianness
+    pub fn read_wide_near_addr(&self, addr: SimpleValue) -> WideValue {
+        let addr = addr as usize;
+        debug_assert!((SIMPLE_ADDR_START + addr + 1) < self.size);
+        return simple_to_wide(&(self.memory[SIMPLE_ADDR_START + addr], self.memory[SIMPLE_ADDR_START + addr + 1]));
+    }
+
+    // TODO: Check endianness
+    pub fn read_wide_far_addr(&self, addr: WideValue) -> WideValue {
+        let addr = addr as usize;
+        debug_assert!(addr + 1 < self.size);
+        return simple_to_wide(&(self.memory[addr], self.memory[addr + 1]));
+
     }
 }
