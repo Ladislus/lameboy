ad16e9cc322b30f7cd41b2bca6d200624e483c9d
Ladislus <walcak.ladislas@gmail.com>
2023-02-12 01:38:06 +0100

    [ADD] Added 'function_path' macro to get full function name

--- src/utils/log.rs
+++ src/utils/log.rs
@@ -1,4 +1,16 @@
 #[macro_export]
+macro_rules! function_path {
+    () => {{
+        fn f() {}
+        fn type_name_of<T>(_: T) -> &'static str {
+            std::any::type_name::<T>()
+        }
+        let name = type_name_of(f);
+        &name[..name.len() - 3]
+    }};
+}
+
+#[macro_export]
 macro_rules! function_name {
     () => {{
         fn f() {}
