f0979cc4ec34077d29fc9ba0cb636f3c7f1d8268
Ladislus <walcak.ladislas@gmail.com>
2023-02-18 01:33:57 +0100

    [ADD] Added Push & Pop functions for the stack

--- src/cpu/memory.rs
+++ src/cpu/memory.rs
@@ -1,4 +1,5 @@
 use crate::cpu::register::RegisterGroup;
+use crate::utils::bits::bit_size;
 use crate::utils::conversions::{pair_to_wide, wide_to_pair};
 use crate::utils::log::log;
 use crate::utils::types::{Byte, FarAddress, NearAddress, Value, WideValue};
@@ -6,7 +7,7 @@ use crate::utils::types::{Byte, FarAddress, NearAddress, Value, WideValue};
 const NEAR_ADDR_START: usize = 0xFF00;
 
 type MemoryPtr = Box<[Byte]>;
-type Stack = Vec<WideValue>;
+type Stack = Vec<Value>;
 
 pub struct Memory {
     pub size: usize,
@@ -32,6 +33,40 @@ impl Memory {
         let far_addr = (addr as usize) + NEAR_ADDR_START;
         log!("MEMORY", format!("Near Address {:#x} + {:#x} = {:#x}", addr, NEAR_ADDR_START, far_addr));
         return far_addr;
+    }
+
+    // TODO: manage stack size
+    pub fn stack_push_wide(&mut self, value: WideValue) {
+
+        let splitted = wide_to_pair(value);
+
+        self.stack[self.registers.SP - 1] = splitted.0;
+        self.stack[self.registers.SP - 2] = splitted.1;
+
+        // Comes from:
+        // http://gameboy.mongenel.com/dmg/opcodes.html
+        //      (SP-1) <-  HIGH(Value), (SP-2) <-  LOW(value), SP <- SP-2
+        self.registers.SP -= 2;
+    }
+
+    // TODO: manage stack size
+    pub fn stack_pop_wide(&mut self) -> WideValue {
+        let low: WideValue = self.stack[self.registers.SP];
+        let high: WideValue = self.stack[self.registers.SP + 1];
+        let value = (high << (bit_size(high) / 2)) + low;
+
+        // Comes from:
+        // >    ld LOW(r16), [sp] ; C, E or L
+        // >    inc sp
+        // >    ld HIGH(r16), [sp] ; B, D or H
+        // >    inc sp
+        // So SP is incremented 2 times
+        // or
+        // http://gameboy.mongenel.com/dmg/opcodes.html
+        // >    	LOW(register) <- (SP), HIGH(register) <- (SP+1), SP <- SP+2
+        self.registers.SP += 2;
+
+        return value;
     }
 
     pub fn write_near_addr(&mut self, addr: NearAddress, value: Value) {
